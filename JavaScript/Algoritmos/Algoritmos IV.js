5b3d658596835fcb727b5bb2ad0b9a35b933b1cd
Tomas <tomascastreb@gmail.com>
2024-05-11 06:48:16 -0300

    Algoritmo IV todas las consignas

--- JavaScript/Algoritmos/Algoritmos IV.js
+++ JavaScript/Algoritmos/Algoritmos IV.js
@@ -50,3 +50,14 @@ function c(a){
 
 var arr=c([-20,-5,6,11])
 console.log(arr)
+
+/*Escribe un codigo que cumpla la siguiente consigna: Dado un array y su respectivo índice, remueve los valores en el rango del índice dado( acortando el array). Por ejemplo, removerRango([20,30,40,50,60,70],2,4)debiera devolver [20,30,70]. */
+
+function removerRango(arr,a,b){
+
+    arr.splice(a,b-a+1)
+return removerRango(arr)
+}
+
+var Y= removerRango([20,30,40,50,60,70],2,4)
+console.log (Y)
